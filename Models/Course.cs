21d1d0abf0b28730c1a0507326258052e8530e30
Naf'an Rizkilah <100104632+riizlaah@users.noreply.github.com>
2026-03-05 14:02:21 +0700

    feat: course management

--- Models/Course.cs
+++ Models/Course.cs
@@ -23,18 +23,46 @@ public partial class Course
     public virtual ICollection<Module> Modules { get; set; } = new List<Module>();
 
     public virtual ICollection<Purchase> Purchases { get; set; } = new List<Purchase>();
+
+    public void updateModules(List<string> inputs)
+    {
+        var i = 0;
+        foreach(var inp in inputs)
+        {
+            if(i > Modules.Count - 1)
+            {
+                Modules.Add(new Module { Title = inp, Content = inp });
+            }
+            else if (Modules.ElementAt(i).Title != inp)
+            {
+                Modules.ElementAt(i).Title = inp;
+            }
+            i += 1;
+        }
+        if(inputs.Count < Modules.Count)
+        {
+            var removedItems = Modules.Skip(i);
+            foreach(var item in removedItems)
+            {
+                Modules.Remove(item);
+            }
+        }
+    }
 }
 
 public class CourseInput
 {
     public string title { get; set; } = null!;
     public string description { get; set; } = null!;
-    public decimal price { get; set; }
+    public Decimal price { get; set; }
     public int duration { get; set; }
-    public string[] modules { get; set; }
+    public List<string> modules { get; set; }
 
     public Course toCourse()
     {
-        return new Course { Title = title, Description = description, Price = price, Duration = duration };
+        var moduleCollections = modules.Select(m => new Module { Title = m, Content = m }).ToList();
+        return new Course { Title = title, Description = description, Price = price, Duration = duration, Modules = moduleCollections };
     }
+    public bool isPriceValid => price > 0.000001m;
+    public bool isDurationValid => duration > 0;
 }
